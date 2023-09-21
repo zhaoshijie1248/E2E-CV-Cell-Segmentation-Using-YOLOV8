8d329bdd85b6d389f652a5ed09f6ea79db495e5b
SHIJIE <59632536+zhaoshijie1248@users.noreply.github.com>
2023-09-20 17:19:06 -0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Shijie Zhao',
+    author_email= 'zhaoshijie1248@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
